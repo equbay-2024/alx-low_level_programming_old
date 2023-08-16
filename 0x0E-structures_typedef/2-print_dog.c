d133ab365388c2ae94f9cac82d38f5633604e34d
equbay-2024 <equbaygebreyowhanes@gmail.com>
2023-08-15 18:31:17 -0700

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,3 +1,13 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - prints a struct dog
+ * @d: pointer to struct type dog
+ * Return: No
+ */
+
+void print_dog(struct dog *d)
 {
 	if (d)
 	{
